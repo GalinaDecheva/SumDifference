32c9f2fe2a6c732c78b4471b9cce3fc0072a853c
Gale <Gale@Galeto>
2017-02-25 17:59:40 +0200

    Ready - only for Int

--- src/SumDifference.java
+++ src/SumDifference.java
@@ -1,4 +1,39 @@
+import java.util.Scanner;
 
 public class SumDifference {
+	public static void main (String[] arg)
+	{
+		Scanner input = new Scanner(System.in);
+	
+		int a;
+		int b;
+		int sum;
+		int dif;
+		int mul;
+		int remDiv;
+		int Div;
+		
+		System.out.print("Enter value: ");
+		a = input.nextInt();
+		System.out.print("Enter value: ");
+		b = input.nextInt();
+		sum = a + b;
+		System.out.println("Sum = " + sum);
+		
+		dif = a - b;
+		System.out.println("Difference = " + dif);
+		
+		mul = a * b;
+		System.out.println("Multiplay = " + mul);
+		
+		remDiv = a % b;
+		System.out.println("Remainder of Div = " + remDiv);
+		
+		Div = a / b;
+		System.out.println("Division = " + Div);
+		
+		input.close();
+		
+	}
 
 }
